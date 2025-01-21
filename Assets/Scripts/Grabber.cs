9cc2e71e383d37ce9fd8ed21dafc3dee516f230a
Damian <damian.gierer@gmail.com>
2025-01-21 14:24:09 +0100

    removed annoying mouse ray

--- Assets/Scripts/Grabber.cs
+++ Assets/Scripts/Grabber.cs
@@ -81,10 +81,4 @@ public class Grabber : MonoBehaviour {
 
         return hit;
     }
-
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.red;
-        Gizmos.DrawRay(Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane)), Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.farClipPlane)) - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane)));
-    }
 }
