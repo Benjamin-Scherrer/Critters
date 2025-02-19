fd1f529b366305d564d4a741cb9d8b955ab86048
Damian <damian.gierer@gmail.com>
2025-02-19 17:40:24 +0100

    osc touch implementation

--- Assets/Scripts/Grabber.cs
+++ Assets/Scripts/Grabber.cs
@@ -1,6 +1,11 @@
+using extOSC;
 using UnityEngine;
 
 public class Grabber : MonoBehaviour {
+
+    [Header("OSC Settings")][Space]
+    [SerializeField] private OSCReceiver Receiver;
+    [SerializeField] private string Address = "/ipad";
 
     [Range(0.2f,1f)]
     [SerializeField] private float grabSlowMotion = 0.8f;
@@ -11,6 +16,8 @@ public class Grabber : MonoBehaviour {
     [SerializeField] private float rotSpring = 20f;
     [SerializeField] private float rotDamp = 5f;
 
+    private Vector3 oscPosition = Vector3.zero;
+    private bool oscDown = false;
     private Vector3 position;
     private Vector3 worldPosition;
     private Rigidbody rb;
@@ -23,19 +30,49 @@ public class Grabber : MonoBehaviour {
         rb = GetComponent<Rigidbody>();
         meshRenderer = GetComponent<MeshRenderer>();
         meshRenderer.enabled = false;
+        Receiver.Bind(Address, MapValues);
     }
 
-    private void Update() {
+    private void Update()
+    {
+        if (oscDown)
+        {
+            Drag(oscPosition, oscDown);
+            Debug.Log(oscPosition + ", " + oscDown);
+        }
+        else
+        {
+            Drag(Input.mousePosition, Input.GetMouseButton(0));
+        }
+    }
 
-        position = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.WorldToScreenPoint(transform.position).z);
+    private void MapValues(OSCMessage message)
+    {
+        var values = message.FindValues(OSCValueType.Float, OSCValueType.True, OSCValueType.False);
+        oscPosition = new Vector3((float)values[0].FloatValue * Screen.width, (float)values[1].FloatValue * Screen.height, 0);
+        oscDown = values[2].BoolValue;
+    }
+
+    private void Drag(Vector3 inputPosition, bool down)
+    {
+        position = new Vector3(inputPosition.x, inputPosition.y, Camera.main.WorldToScreenPoint(transform.position).z);
         worldPosition = Camera.main.ScreenToWorldPoint(position);
 
-        if (Input.GetMouseButton(0)) {
-            if(snappedGeobody == null) {
-                RaycastHit hit = CastRay();
+        if (down)
+        {
+            if (snappedGeobody == null)
+            {
+                RaycastHit hit = CastRay(inputPosition);
 
-                if(hit.collider != null) {
-                    if (!hit.collider.CompareTag("Geobody")) {
+                if (hit.collider != null)
+                {
+                    meshRenderer.enabled = true;
+                    Cursor.visible = false;
+                    Time.timeScale = grabSlowMotion;
+                    transform.position = new Vector3(worldPosition.x, 1f, worldPosition.z);
+
+                    if (!hit.collider.CompareTag("Geobody"))
+                    {
                         return;
                     }
 
@@ -51,34 +88,37 @@ public class Grabber : MonoBehaviour {
                     joint.yDrive = posDrive;
                     joint.zDrive = posDrive;
                     joint.slerpDrive = rotDrive;
-                    meshRenderer.enabled = true;
-                    Cursor.visible = false;
-                    Time.timeScale = grabSlowMotion;
                 }
-            } else {
+            }
+            else
+            {
                 transform.position = new Vector3(worldPosition.x, 1f, worldPosition.z);
             }
         }
 
-        else if (snappedGeobody != null)
+        else 
         {
-            transform.position = new Vector3(worldPosition.x, 0.5f, worldPosition.z);
-            Destroy(joint);
-            snappedGeobody = null;
+            if (snappedGeobody != null)
+            {
+                transform.position = new Vector3(worldPosition.x, 0.5f, worldPosition.z);
+                Destroy(joint);
+                snappedGeobody = null;
+            }
+
             meshRenderer.enabled = false;
             Cursor.visible = true;
             Time.timeScale = 1f;
         }
     }
 
-    private RaycastHit CastRay() {
+    private RaycastHit CastRay(Vector3 inputPosition) {
         Vector3 screenMousePosFar = new Vector3(
-            Input.mousePosition.x,
-            Input.mousePosition.y,
+            inputPosition.x,
+            inputPosition.y,
             Camera.main.farClipPlane);
         Vector3 screenMousePosNear = new Vector3(
-            Input.mousePosition.x,
-            Input.mousePosition.y,
+            inputPosition.x,
+            inputPosition.y,
             Camera.main.nearClipPlane);
         Vector3 worldMousePosFar = Camera.main.ScreenToWorldPoint(screenMousePosFar);
         Vector3 worldMousePosNear = Camera.main.ScreenToWorldPoint(screenMousePosNear);
