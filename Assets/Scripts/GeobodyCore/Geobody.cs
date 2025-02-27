1fdc4d2518da210c87d5efa133c7cff4bbe52ce3
DukeOfOool <156922682+DukeOfOool@users.noreply.github.com>
2025-02-27 17:25:21 +0100

    Revert "Merge branch 'main' into MovementSystem"
    This reverts commit 6a007ea1708a998d438216b88bd54922f5057370, reversing
    changes made to 5784c280679ac918b2a7535ebefe58d72a0a7b34.

--- Assets/Scripts/GeobodyCore/Geobody.cs
+++ Assets/Scripts/GeobodyCore/Geobody.cs
@@ -34,8 +34,6 @@ public class Geobody : MonoBehaviour
     //PROPERTIES
     public ConglomerateManager GetConglomerateHead { get { return conglomerateHead; } } 
 
-    public static int colorCount = 0;
-
     //EVENTS
     //public delegate void GeobodyEventHandler(Geobody geobody);
     //public event GeobodyEventHandler JointSnapped;
@@ -85,9 +83,6 @@ public class Geobody : MonoBehaviour
         conglomerateHead = null;
 
         jointPointSnap.UpdateSnapPointStatus(true, false);
-
-        if (!GeobodyManager.Instance.looseGeobodies.Contains(this)) GeobodyManager.Instance.looseGeobodies.Add(this);
-        if (GeobodyManager.Instance.snappedGeobodies.Contains(this)) GeobodyManager.Instance.snappedGeobodies.Remove(this);
     }
 
     public void SetToMainHead(ConglomerateManager newConglomerateHead, List<IMovementModule> movementModules, float forceAllocated)
@@ -100,9 +95,6 @@ public class Geobody : MonoBehaviour
 
         
         jointPointSnap.UpdateSnapPointStatus(true, true);
-
-        if (!GeobodyManager.Instance.snappedGeobodies.Contains(this)) GeobodyManager.Instance.snappedGeobodies.Add(this);
-        if (GeobodyManager.Instance.looseGeobodies.Contains(this)) GeobodyManager.Instance.looseGeobodies.Remove(this);
     }
 
     public void SetToSideHead(ConglomerateManager newConglomerateHead, List<IMovementModule> movementModules, float forceAllocated, float timeOffset)
@@ -114,9 +106,6 @@ public class Geobody : MonoBehaviour
         conglomerateHead = newConglomerateHead;
 
         jointPointSnap.UpdateSnapPointStatus(true, true);
-
-        if (!GeobodyManager.Instance.snappedGeobodies.Contains(this)) GeobodyManager.Instance.snappedGeobodies.Add(this);
-        if (GeobodyManager.Instance.looseGeobodies.Contains(this)) GeobodyManager.Instance.looseGeobodies.Remove(this);
     }
 
     public void SetToLimb(ConglomerateManager newConglomerateHead)
@@ -128,9 +117,6 @@ public class Geobody : MonoBehaviour
         conglomerateHead = newConglomerateHead;
 
         jointPointSnap.UpdateSnapPointStatus(true, false);
-
-        if (!GeobodyManager.Instance.snappedGeobodies.Contains(this)) GeobodyManager.Instance.snappedGeobodies.Add(this);
-        if (GeobodyManager.Instance.looseGeobodies.Contains(this)) GeobodyManager.Instance.looseGeobodies.Remove(this);
     }
 
 
@@ -157,15 +143,14 @@ public class Geobody : MonoBehaviour
         //
         if(!TryGetComponent<Renderer>(out myRenderer))
             throw new Exception("No Renderer component found in children of Geobody");
+
+        if (myRenderer != null) baseMaterial = myRenderer.material;
     }
 
     private void Start()
     {
         //Set Geobody Type
         SetToSeparate();
-
-        baseMaterial = GeobodyManager.Instance.PickMaterial();
-        myRenderer.material = baseMaterial;
     }
 
     private void Update()
