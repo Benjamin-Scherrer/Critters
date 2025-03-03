c86d0f7cfed0802dd3f5d8660b47f94fd44107a0
GupiDev <michael-staub@outlook.com>
2025-03-03 23:45:54 +0100

    Setup

---
+++ Assets/Scripts/ConnectionPointsDisplay.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConnectionPointsDisplay : MonoBehaviour
+{
+    [SerializeField] private GameObject targetObject; // Assign this in the inspector
+
+    private void Update()
+    {
+        if (targetObject != null)
+        {
+            targetObject.SetActive(!Cursor.visible);
+        }
+    }
+}
