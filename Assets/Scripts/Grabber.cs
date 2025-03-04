349fc7d06893a8b82dfe1001cf0e8e68127d2b98
Damian <damian.gierer@gmail.com>
2025-03-04 16:40:53 +0100

    Revert "curse you cursorrr"
    This reverts commit ca943a94b141995a69905c014d534eb9d314dccf.

--- Assets/Scripts/Grabber.cs
+++ Assets/Scripts/Grabber.cs
@@ -45,7 +45,6 @@ public class Grabber : MonoBehaviour
     {
         mainCam = Camera.main;
         Receiver.Bind(Address, MapValues);
-        Cursor.visible = false;
     }
 
     private void Update()
@@ -87,7 +86,7 @@ public class Grabber : MonoBehaviour
                 if (cursorHit.collider == null) return;
 
                 meshRenderer.enabled = true;
-                //Cursor.visible = false;
+                Cursor.visible = false;
                 Time.timeScale = grabSlowMotion;
 
                 if (geobodyHit.collider == null) return;
@@ -112,7 +111,7 @@ public class Grabber : MonoBehaviour
             }
 
             meshRenderer.enabled = false;
-            //Cursor.visible = true;
+            Cursor.visible = true;
             Time.timeScale = 1f;
         }
     }
