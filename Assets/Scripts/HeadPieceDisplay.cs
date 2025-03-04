810b5ed2aa9650dcd83921637266a685774c9f6e
GupiDev <michael-staub@outlook.com>
2025-03-04 15:21:58 +0100

    Heads

--- Assets/Scripts/HeadPieceDisplay.cs
+++ Assets/Scripts/HeadPieceDisplay.cs
@@ -4,10 +4,23 @@ using UnityEngine;
 
 public class HeadPieceDisplay : MonoBehaviour
 {
-
     [SerializeField] private Geobody geobody;
-    // Start is called before the first frame update
+    [SerializeField] private GameObject displayObject; // The GameObject to activate
 
+    private void FixedUpdate()
+    {
+        if (geobody != null && displayObject != null)
+        {
 
-    
+            if (geobody.GetGeobodyType == GeobodyType.MainHead && !displayObject.activeSelf)
+            {
+                displayObject.SetActive(true);
+            }
+            else if(geobody.GetGeobodyType != GeobodyType.MainHead)
+            {
+                displayObject.SetActive(false);
+            }
+
+        }
+    }
 }
