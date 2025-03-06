e98723d06b9a16fd517076b530a00b9fc343a65c
Damian <damian.gierer@gmail.com>
2025-03-06 11:04:21 +0100

    remove cursor

--- Assets/Scripts/Grabber.cs
+++ Assets/Scripts/Grabber.cs
@@ -59,6 +59,7 @@ public class Grabber : MonoBehaviour
     {
         mainCam = Camera.main;
         Receiver.Bind(Address, MapValues);
+        Cursor.visible = false;
     }
 
     private void Update()
@@ -112,7 +113,7 @@ public class Grabber : MonoBehaviour
                 if (cursorHit.collider == null) return;
 
                 meshRenderer.enabled = true;
-                Cursor.visible = false;
+                //Cursor.visible = false;
                 Time.timeScale = grabSlowMotion;
 
                 if (geobodyHit.collider == null) return;
@@ -137,7 +138,7 @@ public class Grabber : MonoBehaviour
             }
 
             meshRenderer.enabled = false;
-            Cursor.visible = true;
+            //Cursor.visible = true;
             Time.timeScale = 1f;
         }
     }
