436a25d6d141a52e633d2bbfbe7acc8f6598e370
Damian <damian.gierer@gmail.com>
2025-03-07 14:53:17 +0100

    little optimization

--- Assets/Scripts/SnapPull.cs
+++ Assets/Scripts/SnapPull.cs
@@ -6,19 +6,31 @@ public class SnapPull : MonoBehaviour
 {
     [SerializeField] private float pullForce = 10;
 
+
+    Transform geobodyTransform;
+    Geobody geobody;
+    JointPointSnap jointPointSnap;
+
+
+    private void Start()
+    {
+        geobodyTransform = transform.parent.parent;
+        geobody = geobodyTransform.GetComponent<Geobody>();
+        jointPointSnap = geobodyTransform.GetComponent<JointPointSnap>();
+    }
+
     void OnTriggerStay(Collider otherMagnet)
     {
         if (otherMagnet.CompareTag("Magnet"))
         {
+            if (!jointPointSnap.IsPastTImeOfSplitOrSnap) return;
 
-            Transform geobodyTransform = transform.parent.parent;
             Transform otherGeobodyTransform = otherMagnet.transform.parent.parent;
-            Rigidbody otherRigidbody = otherGeobodyTransform.GetComponent<Rigidbody>();
-            Geobody geobody = geobodyTransform.GetComponent<Geobody>();
             Geobody otherGeobody = otherGeobodyTransform.GetComponent<Geobody>();
-            JointPointSnap jointPointSnap = geobodyTransform.GetComponent<JointPointSnap>();
 
             if (jointPointSnap.CheckIfSnappedToSameHirarchy(geobody, otherGeobody)) return;
+
+            Rigidbody otherRigidbody = otherGeobodyTransform.GetComponent<Rigidbody>();
 
             //from snappoint to snappoint
             otherRigidbody.AddForceAtPosition((transform.position - otherMagnet.transform.position).normalized * pullForce, otherMagnet.transform.position);
